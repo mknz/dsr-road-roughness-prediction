d1a7162ca8f6d773bf6a59679348128ba7b96165
mkanazu <mknz@users.noreply.github.com>
2019-02-23 14:10:08 +0100

    Fix cnn model

--- road_roughness_prediction/models/tiny_cnn.py
+++ road_roughness_prediction/models/tiny_cnn.py
@@ -2,32 +2,41 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+def conv_layer(in_planes, out_planes):
+    return nn.Sequential(
+        nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=1, padding=1),
+        nn.BatchNorm2d(out_planes),
+        nn.ReLU(inplace=True),
+        nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+        nn.Dropout(0.2),
+    )
+
+
 class TinyCNN(nn.Module):
     '''Tiny convolutional network for testing purpose'''
+    _fc1_input_size = 128 * 16 * 16
 
     def __init__(self, n_out):
         super(TinyCNN, self).__init__()
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2),
-            nn.Dropout(0.2),
-        )
+        self.conv1 = conv_layer(3, 16)
+        self.conv2 = conv_layer(16, 32)
+        self.conv3 = conv_layer(32, 64)
+        self.conv4 = conv_layer(64, 128)
 
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2),
-            nn.Dropout(0.2),
+        self.fc1 = nn.Sequential(
+            nn.Linear(self._fc1_input_size, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
         )
-
-        self.fc1 = nn.Linear(32 * 64 * 64, 256)
-        self.fc2 = nn.Linear(256, n_out)
+        self.fc2 = nn.Linear(512, n_out)
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.conv2(x)
-        x = x.view(-1, 32 * 64 * 64)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = x.view(-1, self._fc1_input_size)
+        x = self.fc1(x)
+        x = self.fc2(x)
         return x

5a20b39940f55a740a9fdd8049f14078503410f2
mkanazu <mknz@users.noreply.github.com>
2019-02-28 15:46:26 +0100

    Add confusion matrix display etc

--- scripts/eval.py
+++ scripts/eval.py
@@ -12,68 +12,80 @@ from road_roughness_prediction.config import EvalConfig
 from road_roughness_prediction import models
 from road_roughness_prediction.dataset import SurfaceCategoryDatasetFactory
 from road_roughness_prediction.dataset.transformations import TransformFactory
+from road_roughness_prediction.tools import calc_plot_confusion_matrix
 
 
 np.set_printoptions(precision=4)
 
 
-def evaluate(
-        weight_path: Path,
-        data_dir,
-        target_dir_name,
-        categories: List[str],
-        model_name: str,
-        dir_type: str,
-):
-    n_class = len(categories)
+class Evaluator:
+    def __init__(
+            self,
+            weight_path: Path,
+            data_dir,
+            target_dir_name,
+            categories: List[str],
+            model_name: str,
+            dir_type: str,
+    )-> None:
+        self.categories = categories
+        self.n_class = len(categories)
 
-    config = EvalConfig()
-    transform = TransformFactory(config)
+        self.config = EvalConfig()
+        transform = TransformFactory(self.config)
 
-    dataset = SurfaceCategoryDatasetFactory(
-        data_dir,
-        categories,
-        target_dir_name,
-        dir_type,
-        transform,
-    )
-    assert len(dataset), f'No data found in {data_dir}'
-    dataset.show_dist()
-    loader = DataLoader(dataset, batch_size=100)
+        dataset = SurfaceCategoryDatasetFactory(
+            data_dir,
+            categories,
+            target_dir_name,
+            dir_type,
+            transform,
+        )
+        assert len(dataset), f'No data found in {data_dir}'
+        dataset.show_dist()
 
-    if model_name == 'tiny_cnn':
-        net = models.TinyCNN(n_class)
-    elif model_name == 'resnet18':
-        net = models.Resnet18(n_class)
+        self.loader = DataLoader(dataset, batch_size=100)
 
-    net.load_state_dict(state_dict=torch.load(weight_path))
+        if model_name == 'tiny_cnn':
+            self.net = models.TinyCNN(self.n_class)
+        elif model_name == 'resnet18':
+            self.net = models.Resnet18(self.n_class)
 
-    test(net, loader, len(categories))
+        self.net.load_state_dict(state_dict=torch.load(weight_path))
 
+    def evaluate(self, save_fig_path):
+        self.net.eval()
 
-def test(net, loader: DataLoader, n_class):
-    net.eval()
-    class_count = [0 for _ in range(n_class)]
-    class_correct = [0 for _ in range(n_class)]
-    with torch.no_grad():
-        for X, labels in loader:
-            outputs = net.forward(X)
-            _, predicted = torch.max(outputs, 1)
-            for pred, label in zip(predicted.tolist(), labels.tolist()):
-                class_count[int(label)] += 1
-                class_correct[int(label)] += int(pred == label)
+        class_count = [0 for _ in range(self.n_class)]
+        class_correct = [0 for _ in range(self.n_class)]
+        y_test = []
+        y_pred = []
 
-    accuracy = sum(class_correct) / sum(class_count)
-    class_accuracy = [
-        correct / count if count > 0 else 0.
-        for correct, count
-        in zip(class_correct, class_count)
-    ]
+        with torch.no_grad():
+            for X, labels in self.loader:
+                outputs = self.net.forward(X)
+                _, predicted = torch.max(outputs, 1)
+                labels_ = labels.tolist()
+                predicted_ = predicted.tolist()
+                y_test += labels_
+                y_pred += predicted_
+                for label, pred in zip(labels_, predicted_):
+                    class_count[int(label)] += 1
+                    class_correct[int(label)] += int(pred == label)
 
-    print(f'total_accuracy: {accuracy}')
-    print(f'class_accuracy: {class_accuracy}')
-    print(f'class_count: {class_count}')
-    print(f'class_correct: {class_correct}')
+        calc_plot_confusion_matrix(y_test, y_pred, self.categories, save_fig_path)
+
+        accuracy = sum(class_correct) / sum(class_count)
+        class_accuracy = [
+            correct / count if count > 0 else 0.
+            for correct, count
+            in zip(class_correct, class_count)
+        ]
+
+        print(f'total_accuracy: {accuracy}')
+        print(f'class_accuracy: {class_accuracy}')
+        print(f'class_count: {class_count}')
+        print(f'class_correct: {class_correct}')
 
 
 def main():
@@ -84,12 +96,15 @@ def main():
     parser.add_argument('--dir-type', choices=['deep', 'shallow'], default='shallow')
     parser.add_argument('--categories', nargs='+')
     parser.add_argument('--model-name', type=str, default='tiny_cnn')
+    parser.add_argument('--save-fig-path')
     args = parser.parse_args()
 
     data_dir = Path(args.image_dir)
     assert data_dir.exists(), f'Not found {str(data_dir)}'
 
-    evaluate(
+    save_fig_path = Path(args.save_fig_path) if args.save_fig_path else None
+
+    evaluator = Evaluator(
         weight_path=Path(args.weight_path),
         data_dir=data_dir,
         categories=args.categories,
@@ -97,6 +112,7 @@ def main():
         target_dir_name=args.target_dir_name,
         dir_type=args.dir_type,
     )
+    evaluator.evaluate(save_fig_path=save_fig_path)
 
 
 if __name__ == '__main__':
--- tests/test_eval.py
+++ tests/test_eval.py
@@ -1,8 +1,10 @@
 '''Test evaluation code'''
-import os
 from pathlib import Path
 import subprocess
+import shutil
+import tempfile
 
+import pytest
 import torch
 
 from road_roughness_prediction.config import EvalConfig
@@ -10,31 +12,41 @@ from road_roughness_prediction import models
 
 
 class TestEvaluation:
+
     categories = ['asphalt', 'grass']
     n_class = len(categories)
     config = EvalConfig()
 
+    workdir = Path(tempfile.mkdtemp())
+
     # Create dummy weight
     net = models.TinyCNN(n_class)
-    weight_path = Path('/tmp/dummy.pth')
+    weight_path = workdir / 'dummy.pth'
     torch.save(net.state_dict(), weight_path)
 
     image_dir = Path('tests/resources/surfaces')
 
-    def teardown(self):
-        # Delete dummy weight
-        os.remove(Path(self.weight_path))
+    args = [
+        'python3',
+        'scripts/eval.py',
+        '--weight-path', str(weight_path),
+        '--image-dir', str(image_dir),
+        '--model-name', 'tiny_cnn',
+        '--dir-type', 'deep',
+        '--target-dir-name', 'ready',
+        '--categories',
+    ]
+    args += categories
+
+    def teardown_class(self):
+        # Delete temp dir and all of its content
+        shutil.rmtree(self.workdir)
 
     def test_dummy_weight(self):
-        args = [
-            'python3',
-            'scripts/eval.py',
-            '--weight-path', str(self.weight_path),
-            '--image-dir', str(self.image_dir),
-            '--model-name', 'tiny_cnn',
-            '--dir-type', 'deep',
-            '--target-dir-name', 'ready',
-            '--categories'
-        ]
-        args += self.categories
-        subprocess.run(args, check=True)
+        args_ = self.args + ['--save-fig-path', str(self.workdir / 'fig.png')]
+        subprocess.run(args_, check=True)
+
+    @pytest.mark.interactive
+    def test_dummy_weight_with_plot(self):
+        args_ = self.args
+        subprocess.run(args_, check=True)

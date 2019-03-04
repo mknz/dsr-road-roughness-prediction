4f1bea93580798e6a9ded0d9171e0137e037fdc4
mkanazu <mknz@users.noreply.github.com>
2019-03-04 16:14:54 +0100

    Add surface simple category for semantic segmentation

--- road_roughness_prediction/datasets/surface_types.py
+++ road_roughness_prediction/datasets/surface_types.py
@@ -2,7 +2,30 @@
 from enum import Enum
 
 
-class SurfaceBasicCategory(Enum):
+class SurfaceCategoryBase(Enum):
+    '''Surface category base class'''
+
+    @classmethod
+    def get_class_names(cls):
+        return [x.name for x in cls]
+
+    @classmethod
+    def get_class_names_lower(cls):
+        return [x.name.lower() for x in cls]
+
+
+class SurfaceSimpleCategory(SurfaceCategoryBase):
+    '''Surface category, first definition'''
+    ASPHALT = 0
+    GRASS = 1
+    SOIL = 2
+    FLAT_STONES = 3
+    PAVING_STONES = 4
+    SETT = 5
+    BICYCLE_TILES = 6
+
+
+class SurfaceBasicCategory(SurfaceCategoryBase):
     '''Surface category, first definition'''
     ASPHALT = 0
     COMPACTED = 1
@@ -12,11 +35,3 @@ class SurfaceBasicCategory(Enum):
     SETT_COARSE = 5
     SETT_FINE = 6
     SETT_REGULAR = 7
-
-    @classmethod
-    def get_class_names(cls):
-        return [x.name for x in cls]
-
-    @classmethod
-    def get_class_names_lower(cls):
-        return [x.name.lower() for x in cls]

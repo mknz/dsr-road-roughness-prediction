16cc7e8970886ee3910dd12da312da4e17d2d483
mkanazu <mknz@users.noreply.github.com>
2019-03-07 23:00:49 +0100

    Update segmentation training test

--- tests/segmentation/test_training.py
+++ tests/segmentation/test_training.py
@@ -10,14 +10,14 @@ class TestTraining:
     workdir = Path(tempfile.mkdtemp())
 
     image_dir = Path('tests/resources/segmentation/labelme')
-
     args = [
         'python3',
         'scripts/segmentation/train.py',
-        '--data-dirs', str(image_dir), str(image_dir), str(image_dir),
+        '--data-dirs', str(image_dir), str(image_dir),  str(image_dir),
         '--batch-size', '128',
-        '--epochs', '1',
+        '--epochs', '2',
         '--save-dir', str(workdir),
+        '--cpu',
     ]
 
     def teardown_class(self):
@@ -26,4 +26,4 @@ class TestTraining:
 
     def test_unet11(self):
         args_ = self.args + ['--model-name', 'unet11']
-        subprocess.run(args_, check=True)
+        subprocess.run(args_, check=True, timeout=120)

ccd8b150ce1e5aa87808b3a76782d05b101727af
mkanazu <mknz@users.noreply.github.com>
2019-03-26 16:19:00 +0100

    WIP: Uploader and prediction

--- road_roughness_prediction/app/server.py
+++ road_roughness_prediction/app/server.py
@@ -1,4 +1,4 @@
-'''App server'''
+'''Segmentator app server'''
 from functools import wraps
 import time
 
@@ -6,18 +6,27 @@ from flask import Flask
 from flask import flash
 from flask import request
 from flask import redirect
+from flask import make_response
+from flask import render_template
 
 from PIL import Image
 
+from albumentations.augmentations.functional import center_crop
+
+import torch
+from torchvision.transforms.functional import to_pil_image
+from torchvision.transforms.functional import to_tensor
+
 import numpy as np
 
-from road_roughness_prediction.inference import ModelInference
-from road_roughness_prediction.datasets.surface_types import SurfaceBasicCategory
+from road_roughness_prediction.segmentation import logging
+from road_roughness_prediction.segmentation.inference import SidewalkSegmentator
+from road_roughness_prediction.segmentation.datasets import surface_types
+from road_roughness_prediction.tools import torch as torch_tools
+from road_roughness_prediction.segmentation import utils
 
 
-app = Flask(__name__)
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
-model_inf = ModelInference()
 
 
 def allowed_file(filename):
@@ -34,60 +43,117 @@ def measure_time(func):
         retvals = func(*args, **kwargs)
         elapsed = time.time() - start
         times.append(elapsed)
-        print(f'Elapsed: {elapsed:.4f} Average: {np.mean(times):.4f} Std: {np.std(times):.4f}')
+        print(f'Elapsed: {elapsed:.4f} sec Average: {np.mean(times):.4f} sec Std: {np.std(times):.4f} sec')
         return retvals
     return _wrapper
 
 
 @measure_time
-def predict(img):
-    prob = model_inf.predict(img)
-    print(prob)
+def load_segmentator():
+    sidewalk_detector_weight_path = './resources/segmentator/weight_binary.pth'
+    surface_segmentator_weight_path = './resources/segmentator/weight_multi.pth'
+    device = torch.device('cpu')
+    segmentator = SidewalkSegmentator(
+        sidewalk_detector_weight_path=sidewalk_detector_weight_path,
+        surface_segmentator_weight_path=surface_segmentator_weight_path,
+        device=device,
+    )
+    return segmentator
+
+
+def preprocessing(image: np.array) -> torch.Tensor:
+    height, width = 640, 640
+    image = center_crop(image, height, width)
+    image = torch_tools.imagenet_normalize(image)
+    return to_tensor(image).squeeze().unsqueeze(0)
+
+
+@measure_time
+def predict(image, segmentator) -> str:
+    images = preprocessing(image)
+    segmented, sidewalk_mask, background_mask = segmentator.run(images)
+    labels, counts = np.unique(segmented, return_counts=True)
+    n_total = np.prod(segmented.shape)
+
+    def _get_count(index):
+        for label, count in zip(labels, counts):
+            if label == index:
+                return count
+        return 0
+
     results = []
-    for cat in SurfaceBasicCategory:
-        name = cat.name
-        index = cat.value
-        results.append(f'{name:20s} {prob[0, index]:.4f}')
-    return '\n'.join(results)
+    for category in surface_types.SimpleCategory:
+        name = category.name
+        index = category.value
+        count = _get_count(index)
+        percent = 100 * count / n_total
+        results.append(f'{name:20s} {count:09d} {percent:6.2f}%')
+
+    summary = '\n'.join(results)
+
+    out_image = postprocessing(images, segmented, sidewalk_mask, background_mask)
+    return summary, out_image
 
 
-@app.route('/predict', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST':
+def _blend_image(original, segmented):
+    blend = Image.blend(original.convert('RGB'), segmented.convert('RGB'), alpha=0.2)
+    return blend
 
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file_ = request.files['file']
 
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file_.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
+def postprocessing(inputs, segmented, sidewalk_mask, background_mask):
+    input_img = to_pil_image(logging.normalize(inputs[0, ::]))
 
-        if file_ and allowed_file(file_.filename):
-            img = np.array(Image.open(file_))[:, :, :3]
-            result_str = predict(img)
-            return '''
-            <!doctype html>
-            <title>Result</title>
-            <h1>Result</h1>
-            <pre>{}</pre>
-            </html>
-            '''.format(result_str)
+    out_seg_img = segmented[0, ::]
+    out_seg_index_img = utils.create_index_image(out_seg_img)
 
-    return '''
-    <!doctype html>
-    <title>Upload image</title>
-    <h1>Upload image</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Predict>
-    </form>
-    '''
+    blend_output_img = _blend_image(input_img, out_seg_index_img)
 
+    img_ = (sidewalk_mask[0, ::] * 255).astype(np.uint8)
+    img_ = Image.fromarray(img_)
+    img_ = _blend_image(input_img, img_)
+
+    return blend_output_img
+
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+
+    segmentator = load_segmentator()
+
+    @app.route('/', methods=['GET', 'POST'])
+    def upload():
+        if request.method == 'POST':
+
+            # check if the post request has the file part
+            if 'file' not in request.files:
+                flash('No file part')
+                return redirect(request.url)
+            file_ = request.files['file']
+
+            # if user does not select file, browser also
+            # submit an empty part without filename
+            if file_.filename == '':
+                flash('No selected file')
+                return redirect(request.url)
+
+            if file_ and allowed_file(file_.filename):
+                img = np.array(Image.open(file_))[:, :, :3]
+                summary, out_image = predict(img, segmentator)
+                bytes_ = utils.pil_image_to_bytes(out_image, format='JPEG')
+
+                from urllib.parse import quote
+                from base64 import b64encode
+
+                encoded_image = b64encode(bytes_).decode('ascii')
+                out_image_url = f'data:image/jpeg;base64,{quote(encoded_image)}'
+                return render_template('prediction.html', out_image_url=out_image_url, summary=summary)
+
+        return render_template('index.html')
+
+    return app
+
+
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
--- road_roughness_prediction/segmentation/utils.py
+++ road_roughness_prediction/segmentation/utils.py
@@ -1,4 +1,5 @@
 '''Segmentation utils'''
+import io
 from pathlib import Path
 
 import numpy as np
@@ -14,3 +15,11 @@ def create_index_image(img: np.array):
     pil_img = Image.fromarray(img, mode='P')
     pil_img.putpalette(surface_types.COLOR_PALETTE)
     return pil_img
+
+
+def pil_image_to_bytes(image: Image, format):
+    with io.BytesIO() as buf:
+        image.save(buf, format=format)
+        buf.seek(0)
+        bytes_ = buf.read()
+    return bytes_

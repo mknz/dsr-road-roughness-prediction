ccc69a377d545c154c3f7af472476f24bee79a6f
mkanazu <mknz@users.noreply.github.com>
2019-04-04 20:22:42 +0200

    Fix path

--- predict.sh
+++ predict.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-SIDEWALK_DETECTOR_WEIGHT_PATH=./resources/segmentation/weight_binary.pth
-SURFACE_SEGMENTATOR_WEIGHT_PATH=./resources/segmentation/weight_multi.pth
+BINARY_WEIGHT_PATH=./resources/segmentation/weight_binary.pth
+MULTI_WEIGHT_PATH=./resources/segmentation/weight_multi.pth
 python3 eval_two_stage_seg.py\
-    --sidewalk-detector-weight-path $SIDEWALK_DETECTOR_WEIGHT_PATH\
-    --surface-segmentator-weight-path $SURFACE_SEGMENTATOR_WEIGHT_PATH\
+    --sidewalk-detector-weight-path $BINARY_WEIGHT_PATH\
+    --surface-segmentator-weight-path $MULTI_WEIGHT_PATH\
     --image-paths $1\
     --save-path ./out\
     --seed 1\
